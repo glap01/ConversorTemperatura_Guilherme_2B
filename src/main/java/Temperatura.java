afeb1079aeec9659c6aac0b6f7ce980ef06e5742
2024105070010 <2024105070010@aqs-lab-c-36.ifro.local>
2025-04-24 14:40:32 -0400

    terceiro commit - temperatura

--- src/main/java/Temperatura.java
+++ src/main/java/Temperatura.java
@@ -1,4 +1,32 @@
-
 public class Temperatura {
+    //ATRIBUTO PRIVADO PARA ARMAZENAR TEMP - CELSIUS
+    private double tempCelsius;
+    
+    //CONSTRUTOR - CRIA UM OBJETO COM TEMP INCIAL
+    public Temperatura(double tempInicialCelsius){
+        this.tempCelsius = tempInicialCelsius;
+    }
+        
+        //GETTER - PARA LER A TEMPERATURA
+        public double getTemperaturaCelsius(){
+            return tempCelsius;
+    }   
+        
+        //SETTER - PARA VOCÊ MUDAR A TEMP
+        public void setTemperaturaCelsius(double mudarTempCelsius){
+        this.tempCelsius = mudarTempCelsius;
+        }
+        
+        //MÉTODO PARA CONVERTER CELSIUS PARA FAHRENHEIT
+        public double converteFahrenheit(){
+            return (tempCelsius*9/5)+32;
+        }
+        
+        //MÉTODO PARA CONVENSER CELSIUS PARA KELVIN
+        public double converteKelvin(){
+            return tempCelsius+273.15;
+        }
     
 }
+
+    
