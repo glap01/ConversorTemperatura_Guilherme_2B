7a479fbe9b5b296e28ca8900da92631ebc5c737b
glap01 <gminusculi20@gmail.com>
2025-05-14 10:36:01 -0400

    Update Temperatura.java

--- src/main/java/Temperatura.java
+++ src/main/java/Temperatura.java
@@ -1,32 +1,78 @@
 public class Temperatura {
-    //ATRIBUTO PRIVADO PARA ARMAZENAR TEMP - CELSIUS
-    private double tempCelsius;
-    
-    //CONSTRUTOR - CRIA UM OBJETO COM TEMP INCIAL
-    public Temperatura(double tempInicialCelsius){
-        this.tempCelsius = tempInicialCelsius;
+
+    //ATRIBUTOS PRIVADOS
+    private double graus;
+
+    //CONSTRUTOR
+    public Temperatura(double grausT) {
+        this.graus = grausT;
     }
-        
-        //GETTER - PARA LER A TEMPERATURA
-        public double getTemperaturaCelsius(){
-            return tempCelsius;
-    }   
-        
-        //SETTER - PARA VOCÊ MUDAR A TEMP
-        public void setTemperaturaCelsius(double mudarTempCelsius){
-        this.tempCelsius = mudarTempCelsius;
-        }
-        
-        //MÉTODO PARA CONVERTER CELSIUS PARA FAHRENHEIT
-        public double converteFahrenheit(){
-            return (tempCelsius*9/5)+32;
-        }
-        
-        //MÉTODO PARA CONVENSER CELSIUS PARA KELVIN
-        public double converteKelvin(){
-            return tempCelsius+273.15;
-        }
-    
+
+    //GETTER
+    public double getGraus() {
+        return graus;
+    }
+    //SETTER
+
+    public void setGraus(double novoGraUs) {
+        this.graus = novoGraUs;
+    }
 }
 
-    
+CELSIUS 
+public class Celsius extends Temperatura {
+
+    //CONSTRUTOR
+    public Celsius(double grausT) {
+        super(grausT);
+    }
+
+    //METODO PARA CONVERTER DE C PRA K
+    public double converterCpraK() {
+        return getGraus() + 273.15;
+    }
+
+    //METODO PARA CONVERTER DE °C PRA °F  
+    public double converterCpraF() {
+        return (getGraus() * 9 / 5) + 32;
+    }
+
+}
+FAHRENHEIT 
+
+public class Fahrenheit extends Temperatura {
+
+    //CONSTRUTOR
+    public Fahrenheit(double grausT) {
+        super(grausT);
+    }
+
+    //MÉTODO PARA CONVERTER DE °F PARA °C
+    public double converterFpraC() {
+        return (getGraus() - 32) * 5 / 9;
+    }
+
+    // MÉTODO PARA CONVERTER DE °F PARA K
+    public double converterFpraK() {
+        return getGraus() + 273.15;
+    }
+}
+KELVIN 
+
+public class Kelvin extends Temperatura {
+
+    //CONSTRUTOR
+    public Kelvin(double grausT) {
+        super(grausT);
+    }
+
+    //MÉTODO PARA CONVERTER DE K PARA °F
+    public double converterKpraF() {
+        return ((getGraus() - 273.15) * 9 / 5) + 32;
+    }
+
+    // MÉTODO PARA CONVERTER DE K PARA °C
+    public double converterKpraC() {
+        return getGraus() - 273.15;
+    }
+}
